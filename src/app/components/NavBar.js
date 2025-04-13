1cd3445be66b5616af5462c22320e1bd06497524
Yash <yash.manglekar@fime.com>
2025-04-13 22:20:50 +0530

    first commit

---
+++ src/app/components/NavBar.js
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="flex fixed top-[8vh] w-[92vw] rounded-full left-[3vw] bg-white py-3 items-center px-3 justify-between">
+      <div className="logo px-5 flex items-center gap-4">
+        <svg className="size-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path d="m13.511 5.853 4.005-4.117 2.325 2.381-4.201 4.005h5.909v3.305h-5.937l4.229 4.108-2.325 2.334-5.741-5.769-5.741 5.769-2.325-2.325 4.229-4.108H2V8.122h5.909L3.708 4.117l2.325-2.381 4.005 4.117V0h3.473v5.853zM10.038 16.16h3.473v7.842h-3.473V16.16z"></path>
+        </svg>
+        <ul className="flex gap-6">
+          <li>Templates</li>
+          <li>Marketplace</li>
+          <li>Discover</li>
+          <li>Pricing</li>
+          <li>Learn</li>
+        </ul>
+      </div>
+      <div className="flex gap-2">
+        <button className="bg-gray-200 font-semibold py-4 px-7 rounded-md">Log in</button>
+        <button className="bg-gray-900 text-white text-lg py-4 px-7 rounded-full">Sign up free</button>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
